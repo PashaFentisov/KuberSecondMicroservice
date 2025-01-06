58f745867b5b18de7d3992778c5303cef057076c
Pasha Fentisov <pasha16.ua@gmail.com>
2025-01-06 19:08:04 +0200

    add faker library

--- src/main/java/core/kubersecondmicroservice/KuberSecondMicroserviceApplication.java
+++ src/main/java/core/kubersecondmicroservice/KuberSecondMicroserviceApplication.java
@@ -1,7 +1,9 @@
 package core.kubersecondmicroservice;
 
+import com.github.javafaker.Faker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class KuberSecondMicroserviceApplication {
@@ -10,4 +12,8 @@ public class KuberSecondMicroserviceApplication {
         SpringApplication.run(KuberSecondMicroserviceApplication.class, args);
     }
 
+    @Bean
+    public Faker faker(){
+        return new Faker();
+    }
 }

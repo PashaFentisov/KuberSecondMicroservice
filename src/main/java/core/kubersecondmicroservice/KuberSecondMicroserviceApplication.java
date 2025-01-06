0a0f6e50e7248f61e684d9b1d9e79e861898c223
Pasha Fentisov <pasha16.ua@gmail.com>
2025-01-06 19:03:28 +0200

    Initial commit

---
+++ src/main/java/core/kubersecondmicroservice/KuberSecondMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package core.kubersecondmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuberSecondMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KuberSecondMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/core/kubersecondmicroservice/KuberSecondMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package core.kubersecondmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuberSecondMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
